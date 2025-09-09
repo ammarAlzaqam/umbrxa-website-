408218c8b9aac7b4ff4db5c2d92504f269018660
ammarAlzaqam <git@ammaralzaqam.com>
2025-09-09 11:33:19 +0300

    fix aos service card animatino

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -8,18 +8,19 @@ export default function Services() {
       <h1 className="title">Our Services</h1>
 
       {/*//! Services Cards */}
-      <div className="overflow-hidden flex max-md:flex-wrap justify-center items-center max-xs:flex-col gap-5">
+      <div className="flex max-md:flex-wrap justify-center items-center max-xs:flex-col gap-5">
         {services.map(({ Icon, title, description }, index) => (
-          <div
-            key={index}
-            className="min-h-[230px] xs:min-h-[300px] w-[250px] flex justify-evenly items-center text-center flex-col p-6 rounded-2xl shadow-md shadow-secondary-500 bg-dark-2"
-            data-aos={index % 2 === 0 ? "fade-right" : "fade-left"}
-            data-aos-duration={1300}
-
-          >
-            <Icon className="size-15 text-primary-500" />
-            <h3 className="sub-title">{title}</h3>
-            <p className="des line-clamp-3">{description}</p>
+          <div key={index} className="overflow-hidden min-h-[230px] xs:min-h-[300px] w-[250px] p-6 rounded-2xl shadow-box bg-dark-2 flex">
+            <div
+             
+              className="flex flex-col justify-evenly items-center text-center w-full flex-1"
+              data-aos={index % 2 === 0 ? "fade-right" : "fade-left"}
+              data-aos-duration={1300}
+            >
+              <Icon className="size-15 text-primary-500" />
+              <h3 className="sub-title">{title}</h3>
+              <p className="des line-clamp-3">{description}</p>
+            </div>
           </div>
         ))}
       </div>
